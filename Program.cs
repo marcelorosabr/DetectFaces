61410e09bc614a4c860b43dac29614335e1a5bb4
Marcelo Rosa <marcelo.rosa.br@gmail.com>
2019-10-03 20:46:11 -0300

    initial

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DlibDotNet;
+using DlibDotNet.Extensions;
+using Dlib = DlibDotNet.Dlib;
+
+namespace FaceDetector
+{
+    /// <summary>
+    /// The main program class
+    /// </summary>
+    class Program
+    {
+        // file paths
+        private const string inputFilePath = "D:/Develop/DevelopC#/DetectFaces/images/input3.jpg";
+
+        /// <summary>
+        /// The main program entry point
+        /// </summary>
+        /// <param name="args">The command line arguments</param>
+        static void Main(string[] args)
+        {
+            // set up Dlib facedetector
+            using (var fd = Dlib.GetFrontalFaceDetector())
+            {
+                var img = Dlib.LoadImage<RgbPixel>(inputFilePath);
+
+                // find all faces in the image
+                var faces = fd.Operator(img);
+                foreach (var face in faces)
+                {
+                    // draw a rectangle for each face
+                    Dlib.DrawRectangle(img, face, color: new RgbPixel(0, 255, 255), thickness: 4);
+                }
+                // export the modified image
+                Dlib.SaveJpeg(img, "D:/Develop/DevelopC#/DetectFaces/images/output3.jpg");
+
+            }
+            
+        }
+    }
+}
